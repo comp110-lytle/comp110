59c46e7fdb43523da61e1ad31309753f65d224d1
Chiara Sabato <chiarasabato@Chiaras-MBP.lan>
2024-04-16 20:31:19 -0400

    Modified spreadsheet documentation for grading.

--- code/autograder/grading/grading_csv.py
+++ code/autograder/grading/grading_csv.py
@@ -1,6 +1,7 @@
 """Create submission.csv for project grading."""
 
 __author__ = "Kaki Ryan <kakiryan@cs.unc.edu>"
+edited = "Chiara Sabato <csabato@unc.edu>" # added updates to work for single gradescope file of submissions
 
 import os
 import yaml
@@ -10,12 +11,10 @@ from typing import List
 import numpy as np
 
 # set this to be whatever the big gradescope download folder is
-GS_ASSIGNMENT_EXPORT_1: str = 'assignment_1634500_export'
-GS_ASSIGNMENT_EXPORT_2: str = 'assignment_1634507_export'
+GS_ASSIGNMENT_EXPORT: str = 'assignment_4333023_export'
 
 # set this to be whatever the url is when you open the gradescope assignment and click on manage submissions
-GS_SUBMISSIONS_1: str = "https://www.gradescope.com/courses/288698/assignments/1634500/submissions"
-GS_SUBMISSIONS_2: str = "https://www.gradescope.com/courses/288699/assignments/1634507/submissions"
+GS_SUBMISSIONS: str = "https://www.gradescope.com/courses/288698/assignments/1634500/submissions"
 
 # put the list of UTAs here
 utas: list[str] = ["Abigail Kessel", "Adin Zimmerman", "Andrew Zheng", "Aneka Happer", "Aryonna Rice",
@@ -33,9 +32,8 @@ def main() -> None:
     submissions: list[str] = []
     urls: list[str] = []
 
-    subs1: list[str] = os.listdir(GS_ASSIGNMENT_EXPORT_1)
-    subs2: list[str] = os.listdir(GS_ASSIGNMENT_EXPORT_2)
-    all_subs: list[str] = subs1 + subs2
+    subs1: list[str] = os.listdir(GS_ASSIGNMENT_EXPORT)
+    all_subs: list[str] = subs1
 
     for i in range(len(all_subs)):
         submission: str = all_subs[i]
@@ -47,9 +45,8 @@ def main() -> None:
         if 'yml' not in sub_num:
             submissions.append(sub_num)
             if i < len(subs1):
-                urls.append(GS_SUBMISSIONS_1 + "/" + sub_num)
-            else:
-                urls.append(GS_SUBMISSIONS_2 + "/" + sub_num)
+                urls.append(GS_SUBMISSIONS + "/" + sub_num)
+
 
     tuples = list(zip(submissions, urls))
     df = pd.DataFrame(tuples, columns=["Submission", "URL"])
