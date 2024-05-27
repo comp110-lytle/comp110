aab1c75cd120c6eeeb688fc60ab3fbb549bf1f8b
abyrnes1 <alyssanbyrnes@gmail.com>
2024-05-27 12:56:24 -0400

    Added quiz practice problems

--- code/autograder/graders/exercises/ex01_simple_battleship/__main__.py
+++ code/autograder/graders/exercises/ex01_simple_battleship/__main__.py
@@ -9,8 +9,8 @@ configuration = autograde.Configuration(
     src_path="ex01_simple_battleship.py",
     test_path=path.dirname(path.realpath(__file__)),
     pytest_points=80.0,
-    typecheck_points=5.0,
-    lint_points=5.0,
+    typecheck_points=10.0,
+    lint_points=0.0,
     ec_date_1="2022-01-22",
     ec_date_2="2022-01-23"
 )
