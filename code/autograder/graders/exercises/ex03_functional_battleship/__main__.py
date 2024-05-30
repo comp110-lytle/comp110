3e6455e447aa1cd46c3efe8f816039cb1b9b8b95
abyrnes1 <alyssanbyrnes@gmail.com>
2024-05-30 09:00:48 -0400

    Updated ex03 autograder

--- code/autograder/graders/exercises/ex03_functional_battleship/__main__.py
+++ code/autograder/graders/exercises/ex03_functional_battleship/__main__.py
@@ -6,7 +6,7 @@ from os import path
 from grading import autograde
 
 configuration = autograde.Configuration(
-    src_path="exercises/",
+    src_path="ex03_functional_battleship.py",
     test_path=path.dirname(path.realpath(__file__)),
     pytest_points=90.0,
     typecheck_points=5.0,
