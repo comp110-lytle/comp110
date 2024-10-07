d80a23fa0dfaf0733b01afc2c884acdf402807e0
irhinks <irhinks@ncsu.edu>
2024-10-07 10:51:22 -0400

    added Izzi's CL15 slides, added LS15 to agenda

--- code/autograder/graders/exercises/ex04_list_utils/__main__.py
+++ code/autograder/graders/exercises/ex04_list_utils/__main__.py
@@ -7,10 +7,10 @@ configuration = autograde.Configuration(
     src_path="exercises/ex04_utils.py",
     test_path=path.dirname(path.realpath(__file__)),
     pytest_points=80.0,
-    typecheck_points=10.0,
-    lint_points=10.0,
+    typecheck_points=20.0,
+    lint_points=0.0,
     ec_date_1="2022-09-17",
-    ec_date_2="2022-09-18"
+    ec_date_2="2022-09-18",
 )
 
 results = autograde.run(configuration)
