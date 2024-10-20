714a661b0af2d9eacc115b96833c5d4baea2f060
vhunany <vhunany@unc.edu>
2024-10-20 02:55:03 -0400

    fixing point distribution ofr ex06

--- code/autograder/graders/exercises/ex06_dict_utils_f24/__main__.py
+++ code/autograder/graders/exercises/ex06_dict_utils_f24/__main__.py
@@ -6,9 +6,9 @@ from grading import autograde
 configuration = autograde.Configuration(
     src_path="exercises/ex06",
     test_path=path.dirname(path.realpath(__file__)),
-    pytest_points=42.0,
-    typecheck_points=4.0,
-    lint_points=4.0,
+    pytest_points=90.0,
+    typecheck_points=5.0,
+    lint_points=5.0,
     ec_date_1="2022-10-10",
     ec_date_2="2022-10-11"
 )
