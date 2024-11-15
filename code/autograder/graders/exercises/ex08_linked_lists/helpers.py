e3b6fbaf7f707cc15ee96a489d79d0a49afd7a2d
irhinks <irhinks@ncsu.edu>
2024-11-15 08:01:13 -0500

    add cl23

--- code/autograder/graders/exercises/ex08_linked_lists/helpers.py
+++ code/autograder/graders/exercises/ex08_linked_lists/helpers.py
@@ -5,23 +5,23 @@ from typing import Optional
 
 class Node:
     """Generic linked list Node."""
-    data: int
+    value: int
     next: Optional[Node]
 
-    def __init__(self, data: int, next: Optional[Node] = None):
-        self.data = data
+    def __init__(self, value: int, next: Optional[Node] = None):
+        self.value = value
         self.next = next
     
     def __repr__(self) -> str:
         """Produce a string representation of a linked list."""
         if self.next is None:
-            return f"{self.data} -> None"
+            return f"{self.value} -> None"
         else:
-            return f"{self.data} -> {self.next}"
+            return f"{self.value} -> {self.next}"
     
     def __eq__(self, rhs: Optional[Node]) -> bool:
         """Compare deep equality of two linked lists."""
-        if rhs is None or self.data != rhs.data:
+        if rhs is None or self.value != rhs.value:
             return False
         elif self.next is None and rhs.next is not None:
             return False
